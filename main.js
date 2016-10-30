c1b7a5f31b17f89c7b7014e3dc84cdf5caf165ca
Jamie Morris <Jamie Morris>
2016-10-30 21:09:44 +0000

    Added auto spawn harvester from tutorial

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 2) {
+       var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+       console.log('Spawning new harvester: ' + newName);
+    }
+
     let tower = Game.getObjectById('5f835d9b0ee1db1deff3aac8');
     if(tower) {
         let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
