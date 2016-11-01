6a539b2f787616cf0ba1404416370cbdf9362332
Jamie Morris <maloric@hotmail.com>
2016-11-01 15:53:14 +0000

    Fixed harvester

--- role.harvester.js
+++ role.harvester.js
@@ -1,41 +1,29 @@
-var roleBuilder = {
+var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-	    if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('harvesting');
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.say('building');
-	    }
-
-	    if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-
-
-            /*if (targets.length === 0) {
-                targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: object => object.hits < object.hitsMax
-                });
-                targets.sort((a,b) => a.hits - b.hits);
-            }*/
-
-            if(targets.length > 0) {
-                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
-	    }
-	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
+	    if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
-	    }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION
+                             || structure.structureType == STRUCTURE_SPAWN
+                             || structure.structureType == STRUCTURE_TOWER) &&
+                            structure.energy < structure.energyCapacity;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
 	}
 };
 
-module.exports = roleBuilder;
+module.exports = roleHarvester;
